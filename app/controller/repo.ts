d6efea30840c624e1680fd3933c2f17a9bcc7b8d
TaroXin <15029352778@163.com>
2020-09-12 17:32:04 +0800

    feat: 不管了，先搞定1000star以内的，1000以内循环请求10次接口，拿到所有数据🚀

--- app/controller/repo.ts
+++ app/controller/repo.ts
@@ -1,4 +1,5 @@
 import { Controller } from 'egg'
+import { getStarList } from '../utils/star'
 
 export default class RepoController extends Controller {
   /**
@@ -7,35 +8,7 @@ export default class RepoController extends Controller {
    */
   public async starChart() {
     const { ctx, app } = this
-    // 获取该项目的第一个和最后一个 star
-    // 切分时间节点为10份或者可定义的份数
-    // 然后根据时间节点获取
-    const query = `
-      query {
-        repository(name: "vue", owner: "vuejs") {
-          stargazers(first: 100) {
-            edges {
-              starredAt
-            }
-            totalCount
-          }
-        }
-      }
-    `
-    const result = await app.apolloFetch({
-      query,
-    })
-
-    const { errors, data } = result
-    if (errors) {
-      ctx.resFail(errors)
-      return
-    }
-
-    console.log(data)
-
-    ctx.resSucc({
-      result: 'Get it',
-    })
+    const list = await getStarList(app, 'open-source', 'juejin-im')
+    ctx.resSucc(list)
   }
 }
---
+++ app/utils/star.ts
@@ -0,0 +1,58 @@
+import { Application } from 'egg'
+
+/**
+ * 获得 star 数组
+ */
+export async function getStarList(
+  app: Application,
+  repo: string,
+  owner: string,
+  endCursor = '',
+  totalCount = 0
+) {
+  console.log('执行获取Star列表', repo, owner, endCursor)
+  const starList: any[] = []
+  const query = `
+    query($repo: String!, $owner: String!) {
+      repository(name: $repo, owner: $owner) {
+        stargazers(first: 100${endCursor}) {
+          edges {
+            starredAt
+          }
+          pageInfo {
+            endCursor
+          }
+          totalCount
+        }
+      }
+    }
+  `
+
+  const { errors, data } = await app.apolloFetch({
+    query,
+    variables: {
+      repo,
+      owner,
+    },
+  })
+
+  if (errors) {
+    console.log(errors)
+    throw new Error(errors)
+  }
+
+  starList.push(...data.repository.stargazers.edges)
+  const totalStarCount = totalCount || data.repository.stargazers.totalCount
+  if (totalStarCount <= 1000 && starList.length < totalStarCount) {
+    const secondList = await getStarList(
+      app,
+      repo,
+      owner,
+      `, after: "${data.repository.stargazers.pageInfo.endCursor}"`,
+      totalStarCount - 100
+    )
+    console.log(secondList, '获取第二次')
+    starList.push(...secondList)
+  }
+  return starList
+}
